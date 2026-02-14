24aa86bd31b97eec70db011c972097fab672b1cb
AfriN <afrozaafrin124142@gmaile.com>
2026-02-15 01:41:17 +0600

    ready for deployment

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -29,15 +29,16 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = 'django-insecure-w@-(6xs)!=nq&tb!1*xe8rrih!f&7qctn+3588-p0_apav6p)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
 
+    "whitenoise.runserver_nostatic",
     'rest_framework.authtoken',
     'djoser',
     'drf_yasg',
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,7 +85,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Library_management_system.wsgi.application'
+WSGI_APPLICATION = 'Library_management_system.wsgi.app'
 
 
 # Database
@@ -140,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
